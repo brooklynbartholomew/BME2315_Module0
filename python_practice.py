512d43ca737629dc62e3dad3e4ecfd412b520604
Brooklyn Bartholomew <nbx2ue@virginia.edu>
2026-09-03 10:39:08 -0400

    Update python_practice.py
    Added my name.

--- python_practice.py
+++ python_practice.py
@@ -2,6 +2,8 @@
 # As discussed in class, feel free to use AI tools to help you complete this assignment, but remember to cite them.
 # I encourage you to try the problems yourself first and only use AI tools when you are stuck to benefit your learning. 
 
+# Brooklyn Bartholomew
+
 # %% ###########################################################
 # Problem 1: Practice writing pseudocode
 
